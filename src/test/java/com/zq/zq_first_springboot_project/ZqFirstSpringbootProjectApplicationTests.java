5ac11b05d9e09afc6bd224f95bcb3ae0131202fc
BreadZ233 <zq_amazinglife@sina.com>
2020-04-14 22:50:20 +0800

    first commit

---
+++ src/test/java/com/zq/zq_first_springboot_project/ZqFirstSpringbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zq.zq_first_springboot_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZqFirstSpringbootProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
